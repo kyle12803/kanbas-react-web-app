3728c516ee4a6ef62d546124d89a3c54a6ccc148
Kyle C <kyle.4334@yahoo.com>
2023-10-18 15:37:12 -0400

    finished home

--- src/Kanbas/Courses/index.js
+++ src/Kanbas/Courses/index.js
@@ -1,11 +1,13 @@
-import { FaBars } from "react-icons/fa";
+import { FaBars, FaGlasses } from "react-icons/fa";
 import db from "../../Kanbas/Database";
-import Breadcrumb from "react-bootstrap/Breadcrumb";
 import "./index.css";
 import CourseNavigation from "./CourseNavigation.js";
-import { Link, Navigate, Route, Routes, useParams } from "react-router-dom";
+import { Link, Navigate, Route, Routes, useLocation, useParams } from "react-router-dom";
+import Modules from "./Modules";
+import Home from "./Home";
 
 function Courses() {
+	const { pathname } = useLocation();
 	const { courseId } = useParams();
 	const course = db.courses.find((course) => course._id === courseId);
 	return (
@@ -15,42 +17,44 @@ function Courses() {
 					<div className="col-1">
 						<FaBars className="fs-3 text-danger" />
 					</div>
-					<div className="col-10">
+					<div className="col-9">
 						<nav style={{ "--bs-breadcrumb-divider": "'>'" }} aria-label="breadcrumb">
-							<ol class="breadcrumb">
+							<ol class="breadcrumb fs-5">
 								<li class="breadcrumb-item">
 									<Link key={course._id} to="#" className="text-danger">
 										{course.name}
 									</Link>
 								</li>
 								<li class="breadcrumb-item active" aria-current="page">
-									Home
+									{decodeURIComponent(pathname.substring(pathname.lastIndexOf("/") + 1))}
 								</li>
 							</ol>
 						</nav>
 					</div>
+					<div class="col-2">
+						<button
+							type="button"
+							class="btn btn-light btn-outline-secondary text-nowrap d-flex align-items-center"
+						>
+							<FaGlasses />
+							Student View
+						</button>
+					</div>
 					<hr />
 				</div>
 				<div className="row">
-					<CourseNavigation className="col-2 col-lg-2 d-none d-md-block" />
-
-					<div>
-						<div
-							className="overflow-y-scroll position-fixed bottom-0 end-0"
-							style={{
-								left: "320px",
-								top: "50px",
-							}}
-						>
-							<Routes>
-								<Route path="/" element={<Navigate to="Home" />} />
-								<Route path="Home" element={<h1>Home</h1>} />
-								<Route path="Modules" element={<h1>Modules</h1>} />
-								<Route path="Assignments" element={<h1>Assignments</h1>} />
-								<Route path="Assignments/:assignmentId" element={<h1>Assignment Editor</h1>} />
-								<Route path="Grades" element={<h1>Grades</h1>} />
-							</Routes>
-						</div>
+					<div className="col-2 col-lg-2 d-none d-md-block">
+						<CourseNavigation />
+					</div>
+					<div className="col-10">
+						<Routes>
+							<Route path="/" element={<Navigate to="Home" />} />
+							<Route path="Home" element={<Home />} />
+							<Route path="Modules" element={<Modules />} />
+							<Route path="Assignments" element={<h1>Assignments</h1>} />
+							<Route path="Assignments/:assignmentId" element={<h1>Assignment Editor</h1>} />
+							<Route path="Grades" element={<h1>Grades</h1>} />
+						</Routes>
 					</div>
 				</div>
 			</div>
