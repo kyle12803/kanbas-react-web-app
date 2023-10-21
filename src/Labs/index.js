a6ce819940b3057123bd184df9dc6e8592dc81fe
Kyle C <kyle.4334@yahoo.com>
2023-10-21 14:24:05 -0400

    finished state

--- src/Labs/index.js
+++ src/Labs/index.js
@@ -1,10 +1,16 @@
 import Assignment3 from "./a3";
 import Nav from "../Nav";
+import { Navigate, Route, Routes } from "react-router";
+import Assignment4 from "./a4";
 function Labs() {
 	return (
 		<div>
 			<Nav />
-			<Assignment3 />
+			<Routes>
+				<Route path="/" element={<Navigate to="a3" />} />
+				<Route path="a3" element={<Assignment3 />} />
+				<Route path="a4" element={<Assignment4 />} />
+			</Routes>
 		</div>
 	);
 }
